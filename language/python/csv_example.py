5575c8677496c23c8c799c27d3ef98bb15e420f3
xingrui <xingrui@gmail.com>
2016-02-02 12:44:43 +0800

    add csv_example.py

---
+++ language/python/csv_example.py
@@ -0,0 +1,10 @@
+import sys
+import csv
+
+def main():
+    csv_writer = csv.writer(sys.stdout)
+    csv_writer.writerow(['campaign_id','injected_cvr','normal_cvr','injected_install', 'injected_click', 'normal_install', 'normal_click'])
+    csv_writer.writerow([1,2,3,"4",True,False,'7',float(9)])
+
+if __name__ == "__main__":
+    main()
