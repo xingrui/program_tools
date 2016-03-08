3535d8caf18bc24c8a5f65d0731d2ad6e061fe99
xingrui <xingrui@gmail.com>
2016-03-08 13:35:19 +0800

    add grep script.

--- language/awk/awk_pipe.sh
+++ language/awk/awk_pipe.sh
@@ -1,3 +1,15 @@
+awk '{print $0|"grep 12"}' << EOF
+1234
+123
+12
+1
+4321
+432
+43
+4
+21212
+EOF
+
 awk 'BEGIN{ "date" | getline d; print d}' << EOF
 EOF
 #可以在awk中打开一个管道，且同一时刻只能有一个管道存在。通过close()可关闭管道。如：$ awk '{print $1, $2 | "sort" }' test END {close("sort")}。awk把print语句的输出通过管道作为linux命令sort的输入,END块执行关闭管道操作。
