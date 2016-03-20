c798e016021ee8dcaa4ee436d315ca2cd337e25c
xingrui <xingrui@gmail.com>
2016-03-20 18:40:08 +0800

    add void 0

---
+++ language/nodejs/void_0.js
@@ -0,0 +1,10 @@
+console.log(undefined);
+var undefined = "123";
+console.log(undefined);
+
+console.log(void 0);
+console.log(void 123);
+
+console.log(typeof null === 'undefined');
+console.log(typeof undefined === 'undefined');
+console.log(typeof void 0 === 'undefined');
