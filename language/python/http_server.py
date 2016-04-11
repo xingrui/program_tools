2c6458c91f6865d970af6cdb9112387fbbb75238
xingrui <xingrui@gmail.com>
2016-04-11 10:09:30 +0800

    update http_server.py

--- language/python/http_server.py
+++ language/python/http_server.py
@@ -11,7 +11,7 @@ import urlparse
 
 class MyRequestHandler(BaseHTTPRequestHandler):
     def send_reply(self, content):
-        self.send_response(200)
+        self.send_response(200)  
         self.send_header("Content-type", "text/html; charset=%s" % 'utf-8')  
         self.send_header("Content-Length", str(len(content)))  
         self.end_headers()  
@@ -19,41 +19,66 @@ class MyRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         result = urlparse.urlparse(self.path)
-        if result.path != '/campaign':
-            self.send_reply('not valid path')
-            return
         params=urlparse.parse_qs(result.query,True)
         if 'id' not in params:
             self.send_reply('no id in params')
             return
-        campaign_id = params['id'][0]
-        _http = get_http_content(campaign_id).split('\n')
-        _extractor = get_extractor_content(campaign_id).split('\n')
-        diff = difflib.HtmlDiff().make_file(_http, _extractor)
-        self.send_reply(diff)
+        if result.path == '/campaign':
+            try:
+                key_id = params['id'][0]
+                _http = get_http_content('campaign', key_id).split('\n')
+                _extractor = get_extractor_content('campaign', key_id).split('\n')
+                diff = difflib.HtmlDiff(wrapcolumn=60).make_file(_http, _extractor)
+                self.send_reply(diff)
+            except:
+                self.send_reply('not valid key_id:[' + key_id + ']')
+            return
+        if result.path == '/inject_code':
+            try:
+                key_id = params['id'][0]
+                _http = get_http_content('inject_code', key_id).split('\n')
+                _extractor = get_extractor_content('inject_code', key_id).split('\n')
+                diff = difflib.HtmlDiff(wrapcolumn=60).make_file(_http, _extractor)
+                self.send_reply(diff)
+            except:
+                self.send_reply('not valid inject_code_id:[' + key_id + ']')
+            return
+        if result.path == '/channel':
+            try:
+                key_id = params['id'][0]
+                _http = get_http_content('channel', key_id).split('\n')
+                _extractor = get_extractor_content('channel', key_id).split('\n')
+                diff = difflib.HtmlDiff(wrapcolumn=60).make_file(_http, _extractor)
+                self.send_reply(diff)
+            except:
+                self.send_reply('not valid channel:[' + key_id + ']')
+            return
+        self.send_reply('not valid path')
+        return
 
-def get_http_content(campaign_id):
-    campaign_id = '100122'
-    response = GET('http://3ss.mobvista.com/track/campaign/orm/' + campaign_id, params =  {"client_id": "qXJSJzzsGDPdYXR1"}, headers =  {"Authorization": "Basic Ozg5P5nB8qegPBhH"})
-    return json.dumps(json.loads(response.content), indent = 4, sort_keys=True)
+def get_http_content(data_type, key_id):
+    response = GET('http://3ss.mobvista.com/track/' + data_type + '/orm/' + key_id, params =  {"client_id": "qXJSJzzsGDPdYXR1"}, headers =  {"Authorization": "Basic Ozg5P5nB8qegPBhH"})
+    json_obj = json.loads(response.content)
+    if data_type == 'inject_code':
+        return json_obj['code']
+    return json.dumps(json_obj, indent = 4, sort_keys=True)
 
-def get_extractor_content(campaign_id):
+def get_extractor_content(data_type, key_id):
     r = redis.Redis(host = '192.168.1.17', port = 6480, db = 0, password='123456')
-    res = r.get('campaign:%s' % str(campaign_id))
-    return json.dumps(json.loads(res), indent = 4, sort_keys=True)
+    res = r.get('%s:%s' % (data_type, str(key_id)))
+    json_obj = json.loads(res)
+    if data_type == 'inject_code':
+        return json_obj['code']
+    return json.dumps(json_obj, indent = 4, sort_keys=True)
 
 def main():
     try:
-        server = HTTPServer(('', 8080), MyRequestHandler)
+        server = HTTPServer(('', 9999), MyRequestHandler)
         print 'started httpserver...'
         server.serve_forever()
     except KeyboardInterrupt:
         server.socket.close()
     exit(0)
-    _http = get_http_content('100122').split('\n')
-    _extractor = get_extractor_content('100122').split('\n')
-    diff = difflib.HtmlDiff().make_file(_http, _extractor)
-    print diff
 
 if __name__ == "__main__":
     main()
