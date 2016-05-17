32848c07efc8db381ffda824047179ddb0d0068e
xingrui <xingrui@gmail.com>
2016-05-18 06:39:21 +0800

    add date_time_transform.py

--- language/python/date_time_transform.py
+++ language/python/date_time_transform.py
@@ -5,9 +5,15 @@ def date_to_timestamp(time_str):
     timestamp = int(time.mktime(time_array))
     return timestamp
 
+def timestamp_to_date(timestamp):
+    x = time.localtime(timestamp)
+    res = time.strftime('%Y-%m-%d %H:%M:%S',x)
+    return res
+
 def main():
     timestamp = date_to_timestamp('20160517')
     print timestamp
+    print timestamp_to_date(timestamp)
 
 if __name__ == "__main__":
     main()
